9f08c654280924ae01ef74ca649d69f984e10705
Ian Wafula <46840113+IamWafula@users.noreply.github.com>
2024-06-27 11:20:27 -0700

    Added Image generation routine with appropriate wait time

--- backend/routes/generation-routes.js
+++ backend/routes/generation-routes.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 
 const express = require('express');
 const routes = express.Router()
-
-routes.get('/:image_id', async (req, res) => {
-    const image_id = req.params.image_id;
-
+/*
+    Function to check how long to wait for the image to finish generating from AI Horde
+*/
+async function getImageWaitTime(image_id){
     const options = {
         method: "GET",
         headers: {
@@ -19,13 +19,50 @@ routes.get('/:image_id', async (req, res) => {
 
     const response = await fetch(url , options);
 
-    if (response.status == 200){
-        const resJson = await response.json()
 
-        return res.json(resJson)
+    if (response.status == 200){
+        const resJson = await response.json();
+        console.log(resJson)
+
+        return resJson.wait_time
+
     }
 
-    return res.json({"response" : "Something went wrong"})
+    return null
+}
+
+async function getImageUrl(image_id){
+    const options = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "apikey": process.env.HORDE_API_KEY
+        }
+    }
+
+    const url = `https://stablehorde.net/api/v2/generate/status/${image_id}`
+
+    const response = await fetch(url , options);
+    if (response.status == 200){
+        const resJson = await response.json();
+        console.log(resJson)
+        if (resJson.generations.length > 0){
+            return resJson.generations[0].img
+        }
+
+    }
+
+    return null
+}
+
+routes.get('/:image_id', async (req, res) => {
+    const image_id = req.params.image_id;
+
+    const url = await getImageUrl(image_id);
+
+    if (url) { return url }
+
+    return res.json({"response" : "Image not ready"})
 
 })
 
@@ -40,7 +77,7 @@ routes.post('/', async (req, res) => {
         "nsfw" : false
     }
 
-    const options = {
+    let options = {
         method: "POST",
         headers: {
             'accept': 'application/json',
@@ -53,13 +90,45 @@ routes.post('/', async (req, res) => {
     const url = "https://stablehorde.net/api/v2/generate/async"
 
     const response = await fetch(url , options)
+
+    const postTimeout = (waitTime, id) => {
+        // wait for image to finish generating, could routine to send email and add to Prisma Schema
+        setTimeout(async ()=> {
+            const imageUrl = await getImageUrl(id);
+            console.log(imageUrl)
+        }, parseInt(waitTime*1000))
+
+        const response = {
+            "id": id,
+            "wait_time" : waitTime
+        }
+
+        return res.json(response)
+    }
+
+    // check if Image is being generated
     if (response.status == 202){
         const resJson = await response.json()
-        console.log(resJson)
+        const imageId = resJson["id"]
 
-        return res.json(resJson)
+        let waitTime = await getImageWaitTime(imageId)
+
+        // slight error when image has just been uploaded for generation, where wait time is 0
+        // set timeout to 2 seconds since 1s still didnt work
+        if (waitTime == 0) {
+            setTimeout(async ()=> {
+                waitTime = await getImageWaitTime(imageId)
+
+                // since repeating same operations twice, moved to function instead
+                return postTimeout(waitTime, imageId)
+            }, 2000)
+
+        } else {
+            return postTimeout(waitTime, imageId)
+        }
+    } else {
+        return res.json({"response" : "Something went wrong"})
     }
-    return res.json({"response" : "Something went wrong"})
 
 })
 
